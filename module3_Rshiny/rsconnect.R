b95d99d86a646f639e90cd29f91721ad93759201
Jonquil <zhonglingliao@MacBook-Pro.local>
2020-12-02 16:09:26 -0600

    tidy up repo

--- module3_Rshiny/rsconnect.R
+++
@@ -1,5 +0,0 @@
-library(rsconnect)
-rsconnect::setAccountInfo(name='johanzhou',
-                          token='BEFACF13D89735315F1F7480C690E701',
-                          secret='9oCUj94GSSVGpAv6InTQSg/sCCW+8BZsCS+voQVb')
-rsconnect::deployApp()
